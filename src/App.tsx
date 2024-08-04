5dcb850cb9e6f172303a7ee87647cc46c9ed293e
Omid Shabab <omidshababdev@gmail.com>
2024-08-04 01:32:39 -0700

    0.1.0

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/header";
+import Hello from "./components/hello";
+
+function App() {
+  return (
+    <div className="w-full">
+      <Header />
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/hello.tsx
@@ -0,0 +1,13 @@
+function Hello() {
+  return (
+    <div className="flex items-center justify-center flex-wrap p-6">
+      <div className="flex items-center flex-shrink-0 text-gray-600 mr-6">
+        <span className="font-semibold text-3xl">
+          👋 Hello from Timeless.co
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Hello;
